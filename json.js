f85ad54b82c8065a7df29c80df80a5d7a74f8bfa
Rolando Velasco <rvelasco@fullsail.edu>
2013-01-23 23:36:19 -0800

    Created JSON file with data.

---
+++ json.js
@@ -0,0 +1,47 @@
+// JSON Data Structure
+
+var json = {
+	"hero": [
+		{
+			"name": "Sir Lot the Brave",
+			"HP": 30,
+			"attack": 5,
+			"haveItems": true,
+			"items": {
+				"potion": [1,10],
+				"rage": [1, 5],
+				"revive": [1, 30]			
+			}
+		}		
+	],
+	"enemies": [
+		{
+			"name": "worm",
+			"HP": 5,
+			"attack": 2,
+			"haveItems": false,
+			"items": {}
+		},
+		{
+			"name": "spider",
+			"HP": 7,
+			"attack": 4,
+			"haveItems": false,
+			"items": {}
+		},
+		{
+			"name": "scorpion",
+			"HP": 10,
+			"attack": 5,
+			"haveItems": false,
+			"items": {}
+		},
+		{
+			"name": "thief",
+			"HP":  20,
+			"attack": 7,
+			"haveItems": false,
+			"items": {}
+		}
+	]
+};
